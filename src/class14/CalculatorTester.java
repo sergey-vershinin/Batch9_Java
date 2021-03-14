1efe6f7501aec200318fce4dcf37739f5ed13dd0
sergey-vershinin <sergey.v.v@mail.com>
2021-03-14 11:32:33 -0400

    clas14.1

---
+++ src/class14/CalculatorTester.java
@@ -0,0 +1,15 @@
+package class14;
+
+public class CalculatorTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
+		
+		System.out.println(calculator.add(10,12));
+		
+		double results=calculator.sub(15.3, 34.5);
+		System.out.println(results);
+	}
+
+}
